6458421a4d2e1e20ef1dcf8d315aaeced7589ba9
Ambria Cr <ambria.creary@baruchmail.cuny.edu>
2026-03-27 03:44:38 +0000

    fixing zip code

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -13,7 +13,7 @@ WITH all_locations AS (
    -- Get locations from restaurant applications
    SELECT DISTINCT
        borough,
-       zip AS zip_code
+       zip_code AS zip_code
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
