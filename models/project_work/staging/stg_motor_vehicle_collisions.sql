155e20110b9ca2ed1b3545d70a3ad451335b623c
Ambria Cr <ambria.creary@baruchmail.cuny.edu>
2026-05-03 00:22:15 +0000

    fxing staging

--- models/project_work/staging/stg_motor_vehicle_collisions.sql
+++ models/project_work/staging/stg_motor_vehicle_collisions.sql
@@ -10,17 +10,6 @@ WITH source AS (
 cleaned AS (
 
     SELECT
-        * EXCEPT (
-            collision_id,
-            crash_date,
-            crash_time,
-            borough,
-            zip_code,
-            latitude,
-            longitude,
-            number_of_persons_injured,
-            number_of_persons_killed
-        ),
 
         -- Identifiers
         CAST(collision_id AS STRING) AS collision_id,
@@ -39,12 +28,13 @@ cleaned AS (
             ELSE 'UNKNOWN'
         END AS borough,
 
-        -- Zip cleanup
+        -- Zip cleanup (NOW SAFE AND EXPLICIT)
         CASE
+            WHEN zip_code IS NULL THEN NULL
             WHEN UPPER(TRIM(CAST(zip_code AS STRING))) IN ('N/A', 'NA', 'ANONYMOUS') THEN NULL
             WHEN LENGTH(CAST(zip_code AS STRING)) = 5 THEN CAST(zip_code AS STRING)
             WHEN LENGTH(CAST(zip_code AS STRING)) = 9 THEN SUBSTR(CAST(zip_code AS STRING), 1, 5)
-            WHEN LENGTH(CAST(zip_code AS STRING)) = 10 AND REGEXP_CONTAINS(CAST(zip_code AS STRING), r'^\d{5}-\d{4}') THEN SUBSTR(CAST(zip_code AS STRING), 1, 5)
+            WHEN REGEXP_CONTAINS(CAST(zip_code AS STRING), r'^\d{5}-\d{4}') THEN SUBSTR(CAST(zip_code AS STRING), 1, 5)
             ELSE NULL
         END AS zip_code,
 
