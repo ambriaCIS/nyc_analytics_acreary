ed549096813cb4ddc3407977a0445d3d6fc78feb
Ambria Cr <ambria.creary@baruchmail.cuny.edu>
2026-05-12 21:24:42 +0000

    fixing

--- models/project_work/marts/shared/dim_vlocation.sql
+++ models/project_work/marts/shared/dim_vlocation.sql
@@ -3,7 +3,7 @@
 WITH locations AS (
     SELECT DISTINCT
         coalesce(borough, 'UNKNOWN') AS borough,
-        coalesce(zip_code, 'UNKNOWN') AS zip_code
+        coalesce(incident_zip, 'UNKNOWN') AS zip_code
     FROM {{ ref('stg_nyc_311_service_request_history') }}
 
     UNION DISTINCT
@@ -17,5 +17,5 @@ WITH locations AS (
 SELECT
     {{ dbt_utils.generate_surrogate_key(['borough','zip_code']) }} AS location_sk,
     borough,
-    incident_zip
+    zip_code
 FROM locations
